33c7b6fd458ee34ae98f0f13650c67e133f2c749
quintongillanders <quingillanders@gmail.com>
2025-09-05 17:22:50 +1200

    Update settings.py

--- room_reservation/room_reservation/settings.py
+++ room_reservation/room_reservation/settings.py
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'room_reservation.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-if os.environ.get('DJANGO_DEBUG', 'True') == 'True':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+  }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
