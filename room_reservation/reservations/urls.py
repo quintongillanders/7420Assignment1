05115b10e43fccd94e436f42f402202f59241704
quintongillanders <quingillanders@gmail.com>
2025-09-09 14:21:13 +1200

    Removed password reset templates and urls

--- room_reservation/reservations/urls.py
+++ room_reservation/reservations/urls.py
@@ -16,12 +16,6 @@ urlpatterns = [
     path('admin-reserve/', views.admin_make_reservation, name='admin_make_reservation'),
     path('reservations/', views.view_all_reservations, name='admin_reservations'),
     path('admin-reservation/cancel/<int:reservation_id>/', views.admin_cancel_reservation, name='admin_cancel_reservation'),
-    path("password_reset/", auth_views.PasswordResetView.as_view(
-        template_name="registration/password_reset_form.html"
-    ), name="password_reset"),
-    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
-    path("reset/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
     path('users/', views.user_list, name='user_list'),
     path('user/<int:user_id>/edit/', views.edit_user, name='edit_user'),
     path('user/<int:user_id>/delete/', views.delete_user, name='delete_user'),
